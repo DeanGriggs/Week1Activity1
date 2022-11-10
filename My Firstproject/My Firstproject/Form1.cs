d75e42cd9bcb8dec8eae704419595092867fe571
DeanGriggs <117603385+DeanGriggs@users.noreply.github.com>
2022-11-09 20:01:25 -0700

    Add files via upload

---
+++ My Firstproject/My Firstproject/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_Firstproject
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            String firstName = tbFirstName.Text;
+            String lastName = tbLastName.Text;
+            lbWelcome.Text = "Hello " + firstName + " " + lastName;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbFirstName.Text = "";
+            tbLastName.Text = "";
+            lbWelcome.Text = "";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lbWelcome.Text = "";
+        }
+    }
+}
